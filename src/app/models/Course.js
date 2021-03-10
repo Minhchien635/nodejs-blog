0aa35c3dcc2d04efc2761c57d7e2a17dc92b0604
Minhchien635 <minhchien0407.vk.1718@gmail.com>
2021-03-10 21:54:05 +0700

    CRUD

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,26 @@
 const mongoose =require('mongoose');
 const Schema = mongoose.Schema;
+const slug = require('mongoose-slug-generator');
+const mongooseDelete = require('mongoose-delete');
 
 const Course = new Schema({
-    name: { type: String, maxLength: 255 },
-    description: { type: String, maxLength: 600},
-    image: { type: String, maxLength: 255 },
-    createdAt: { type: Date, default: Date.now},
-    updatedAt: { type: Date, default: Date.now},
+    name: { type: String, require: true, },
+    description: { type: String},
+    image: { type: String},
+    videoId: { type: String, require: true, },
+    level: { type: String},
+   //createdAt: { type: Date, default: Date.now},
+    //updatedAt: { type: Date, default: Date.now},
+    slug: { type: String, slug: 'name', unique: true},
+},{
+    timestamps: true
+});
+
+//Add plugins
+mongoose.plugin(slug);
+Course.plugin(mongooseDelete, {
+    deletedAt : true,
+    overrideMethods: 'all',
 });
 
 module.exports = mongoose.model('Course', Course);
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -5,6 +5,11 @@ const courseController = require('../app/controllers/CourseController');
 
 //newController.index
 
+router.get('/create', courseController.create);
+router.post('/store', courseController.store);
+router.get('/:id/edit', courseController.edit);
+router.put('/:id', courseController.update);
+router.delete('/:id', courseController.delete);
 router.get('/:slug', courseController.show);
 
 module.exports = router;
