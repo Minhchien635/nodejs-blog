6d439251e386ba4062c4fb7dc5c0500769250e4b
minhchien <minhchien0407.vk.1718@gmail.com>
2021-07-09 14:02:44 +0700

    remove node_modules folder

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -3,6 +3,7 @@ const { mongooseToObject } = require('../../util/mongoose');
 
 class CourseController {
     // [GET] /courses/:slug
+    // [GET] /courses/:slug
     show(req, res, next) {
         Course.findOne({slug: req.params.slug})   
             .then(course => {
