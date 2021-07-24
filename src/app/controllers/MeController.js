5b0e1a565938a05f9a8a8d8f4ce5438d5a4faf6c
minhchien <minhchien0407.vk.1718@gmail.com>
2021-07-24 09:53:45 +0700

    authentication

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -7,7 +7,7 @@ class MeController {
     async storesPosts(req, res, next) {
         const user = await authentication.verify(req, res, next);
         if(user){
-            Promise.all([Post.find({user_id: user._id}), Post.countDocumentsDeleted()])
+            Promise.all([Post.find({user_id: user._id}), Post.countDocumentsDeleted({user_id: user._id})])
                 .then(([posts, deletedCount]) => 
                     res.render('me/stored-posts',{
                         deletedCount,
@@ -34,12 +34,15 @@ class MeController {
     }
 
     // [GET] /me/trash/courses
-    trashPosts(req,res,next){
-        Post.findDeleted({})
-            .then(posts => res.render('me/trash-posts',{
-                posts: mutipleMongooseToObject(posts)
-            }))
-            .catch(next);
+    async trashPosts(req,res,next){
+        const user = await authentication.verify(req, res, next);
+        if(user){
+            await Post.findDeleted({user_id: user._id})
+                .then(posts => res.render('me/trash-posts',{
+                    posts: mutipleMongooseToObject(posts)
+                }))
+                .catch(next);
+        }
     }
 
 }
